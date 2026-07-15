3147b721e4bd04ae5448804b6823d42259a96d7e
Shris <shris@gmail.com>
2026-07-15 17:20:21 +0530

    Upadating third party app

--- DJANGO_AI_INTEGRATE/third_app.py
+++ DJANGO_AI_INTEGRATE/third_app.py
@@ -4,21 +4,21 @@ import json
 # Django server ka URL (Make sure your view URL matches this path)
 URL = "http://127.0.0.1:8000/api/groq-chat/"  # Agar aapne urls.py me 'api/gemini-chat/' rakha hai toh yahi rehne dein
 
-print("🤖 AI Chat Console me aapka swagat hai!")
-print("Type krein aur Enter dabayein. Chat band karne ke liye 'exit' ya 'quit' likhein.\n")
+print("🤖 Welcome To AI Chat Console !")
+print("Enter Prompt and press Enter . To close the chat type 'exit' or 'quit'.\n")
 
 while True:
     # 🔄 User se dynamically input lena
-    user_input = input("✍️  Aapka Sawal: ")
+    user_input = input("✍️  Your Prompt: ")
     
     # Loop se bahar nikalne ki condition
     if user_input.lower() in ['exit', 'quit']:
-        print("\n👋 Chat band ho rahi hai. Bye!")
+        print("\n👋 Chat is closing. Bye!")
         break
         
     # Agar user bina kuch type kiye enter daba de
     if not user_input.strip():
-        print("⚠️  Krpya kuch type karein!")
+        print("⚠️  Prompt something!")
         continue
 
     # Bheje jaane wala data dynamically update ho gaya
@@ -26,7 +26,7 @@ while True:
         "prompt": user_input
     }
 
-    print("🚀 Django API ko request bhej rahe hain...")
+    print("🚀 Django is sending request to API")
 
     try:
         # POST request bhej rahe hain JSON data ke sath
@@ -39,15 +39,15 @@ while True:
         # Status code check kar rahe hain
         if response.status_code == 200:
             result = response.json()
-            print("\n✅ AI ka jawab:")
+            print("\n✅ AI response:")
             print("-" * 50)
             print(result.get("response"))
             print("-" * 50)
             print("\n") # New line space ke liye
         else:
-            print(f"\n❌ Error Aaya! Status Code: {response.status_code}")
+            print(f"\n❌ Error ! Status Code: {response.status_code}")
             print("Response:", response.text)
 
     except requests.exceptions.ConnectionError:
-        print("\n❌ Connection Error: Kya aapka Django server chal raha hai? Pehle 'python manage.py runserver' chalayein.")
+        print("\n❌ Connection Error: Does your django server is running? Run 'python manage.py runserver' first.")
         break
