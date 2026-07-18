542ddbf7f3218ded43e085ca1dbbc48aca92e75e
Shris <shris@gmail.com>
2026-07-18 19:43:06 +0530

    learning session authentication

---
+++ SESSION_AUTHENTICATION/api/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import AllowAny,IsAuthenticated,DjangoModelPermissions,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly,DjangoObjectPermissions
+
+# Create your views here.
+
+class StudentAPI(viewsets.ModelViewSet):
+    authentication_classes=[SessionAuthentication]
+    # permission_classes=[AllowAny]
+    # permission_classes=[IsAuthenticated]
+    # permission_classes=[IsAuthenticatedOrReadOnly]
+    # permission_classes=[IsAdminUser]
+    # permission_classes=[DjangoModelPermissions]
+    # permission_classes=[DjangoModelPermissionsOrAnonReadOnly]
+    permission_classes=[DjangoObjectPermissions]
+    
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+
+class StudentAPI_1(viewsets.ReadOnlyModelViewSet):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
