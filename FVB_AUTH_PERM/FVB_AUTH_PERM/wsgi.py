ab1edc7ca222d5f13512aa839c60c13d1b6c179a
Shris <shris@gmail.com>
2026-07-18 22:53:02 +0530

    Learning FVB Authentication.

---
+++ FVB_AUTH_PERM/FVB_AUTH_PERM/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for FVB_AUTH_PERM project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FVB_AUTH_PERM.settings')
+
+application = get_wsgi_application()
---
+++ FVB_AUTH_PERM/api/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view,authentication_classes,permission_classes
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+# Create your views here.
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def student_api(request, student_id=None):
+    if request.method == 'GET':
+        id=student_id
+        if id is not None:
+            student = Student.objects.get(id=id)
+            serializer = StudentSerializer(student)
+            return Response(serializer.data)
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+        
+    
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Student created successfully'}, status=201)
+        return Response(serializer.errors, status=400)
+    
+    elif request.method == 'PUT':
+        student_id = student_id
+        student = Student.objects.get(id=student_id)
+        serializer = StudentSerializer(student, data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Student updated successfully'})
+        return Response(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        student_id = student_id
+        student = Student.objects.get(id=student_id)
+        student.delete()
+        return Response({'msg': 'Student deleted successfully'}, status=204)  
+    return Response(serializer.errors, status=405)  
+        
