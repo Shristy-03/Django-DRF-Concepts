c52b499b5aa8a3c78166a4b42201282d42393933
Shris <shris@gmail.com>
2026-07-24 20:38:52 +0530

    learning JWT

---
+++ JWT/JWT/urls.py
@@ -0,0 +1,44 @@
+"""
+URL configuration for MODEL_VIEW_SET project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from api import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
+
+router=DefaultRouter()
+router.register('StudentAPI',views.StudentAPI,basename='Student')
+
+router_1=DefaultRouter()
+router_1.register('StudentAPI_1',views.StudentAPI_1,basename='Read_student')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(router.urls)),
+    path('api/',include(router_1.urls)),
+    path('auth/',include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
---
+++ JWT/api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+# Create your views here.
+
+class StudentAPI(viewsets.ModelViewSet):
+    authentication_classes=[JWTAuthentication]
+    permission_classes=[IsAuthenticated]
+    
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+
+class StudentAPI_1(viewsets.ReadOnlyModelViewSet):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
