3ef6b2e8a74f1b3d1e4954d1539c7fd07316476a
Shris <shris@gmail.com>
2026-07-25 22:20:27 +0530

    learning Throttling part-1

---
+++ THROTTLE/api/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import AllowAny,IsAuthenticated,DjangoModelPermissions,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly,DjangoObjectPermissions
+from rest_framework.throttling import AnonRateThrottle,UserRateThrottle,ScopedRateThrottle
+from .throttling import jackThrottle
+# Create your views here.
+
+class StudentAPI(viewsets.ModelViewSet):
+    authentication_classes=[SessionAuthentication]
+    permission_classes=[IsAuthenticatedOrReadOnly]
+    throttle_classes=[AnonRateThrottle,jackThrottle]
+    
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+
+class StudentAPI_1(viewsets.ReadOnlyModelViewSet):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
