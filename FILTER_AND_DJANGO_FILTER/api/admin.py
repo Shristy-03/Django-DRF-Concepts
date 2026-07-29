7a114d3ac4be1dd874a50c3ca333c81a68828b2e
Shris <shris@gmail.com>
2026-07-29 20:15:45 +0530

    learning general filtering

---
+++ FILTER_AND_DJANGO_FILTER/api/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'roll_no', 'city','passby']
+    list_filter = ['city']
+    search_fields = ['name', 'roll_no']
+
+
