9a5728684fa61003503c13b7f02e5f31c686c5b0
Shris <shris@gmail.com>
2026-07-30 20:19:03 +0530

    learning search filter

---
+++ SEARCH_FILTER/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
+
+
+# Create your views here.
+class StudentGet(ListAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    filter_backends=[SearchFilter]
+    search_fields=['^name',]
